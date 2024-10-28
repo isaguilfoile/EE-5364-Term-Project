119a6b8436f5c3293f8a4a35bfed296bd239cf17
Isaiah Guilfoile <Isaiah.Guilfoile@polaris.com>
2024-10-28 11:59:44 -0500

    Replacement policies from research paper

--- Term_Proj/cpp/modules/replacement/eci/eci.cc
+++ Term_Proj/cpp/modules/replacement/eci/eci.cc
@@ -1,2 +1,4 @@
 /* Achieving Non-Inclusive Cache Performance with Inclusive Caches: Temporal Locality Aware (TLA) Cache Management Policies
    (https://ieeexplore.ieee.org/document/5695533) */
+
+// Early Core Invalidation
---
+++ Term_Proj/cpp/modules/replacement/tlh/tlh.cc
@@ -0,0 +1,4 @@
+/* Achieving Non-Inclusive Cache Performance with Inclusive Caches: Temporal Locality Aware (TLA) Cache Management Policies
+   (https://ieeexplore.ieee.org/document/5695533) */
+
+// Temporal Locality Hints (TLH)

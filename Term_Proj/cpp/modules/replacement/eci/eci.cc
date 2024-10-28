efa5726a74f0fac87fd4109638e360ccd11d77c1
Isaiah Guilfoile <Isaiah.Guilfoile@polaris.com>
2024-10-28 12:30:31 -0500

    Added replacement Policy descriptions

--- Term_Proj/cpp/modules/replacement/eci/eci.cc
+++ Term_Proj/cpp/modules/replacement/eci/eci.cc
@@ -2,3 +2,13 @@
    (https://ieeexplore.ieee.org/document/5695533) */
 
 // Early Core Invalidation
+
+/**
+ * TODO: Early Core Invalidation (ECI) derives the temporal locality of a line
+ * before it becomes LRU in the LLC. The LLC choses a line early and invalidates
+ * that line in the core caches, while leaving the line in the LLC. By observing
+ * the core's subsequent request, the LLC derives the temporal locality.
+ *
+ *
+ * See (Section B)
+ */
--- Term_Proj/cpp/modules/replacement/tlh/tlh.cc
+++ Term_Proj/cpp/modules/replacement/tlh/tlh.cc
@@ -2,3 +2,14 @@
    (https://ieeexplore.ieee.org/document/5695533) */
 
 // Temporal Locality Hints (TLH)
+
+/**
+ * TODO:
+ * Temporal Locality Hints (TLH) convey the temporal locality of "hot" lines
+ * in the core caches by sending hits to the LLC to update its replacement state.
+ * With the same temporal informaiton as the core caches, the LLC is significantly
+ * less likely to choose a victim that will cause an inclusion victim.
+ *
+ *
+ * See (Section A)
+ */
